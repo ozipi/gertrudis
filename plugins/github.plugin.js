88aadc28be0c9434585bf4a5270419255a4a7afa
Ozipi <ozipi.nomad@gmail.com>
2012-01-24 23:57:51 -0600

    Added basic github plugin

--- plugins/github.plugin.js
+++ plugins/github.plugin.js
@@ -1,16 +1,23 @@
-var githubInfo = {};
-
-function GithubPlugin (options){
-	console.log('plugin::constructor->');	
-	githubInfo = new Github(options.user);
-};
-
-function execute (options){
-	console.log('plugin::excute->', options);
-	githubInfo.getRepoTags(options.user, options.repo, $.proxy(_getRepoTags_successHandler, this));					
-};
-
-function _getRepoTags_successHandler(result){
-	console.log('plugin::getRepoTags->', result);
-	//return result;
-};
+function GithubPlugin (login){
+	this.init = function(){
+		console.log('GithubPlugin::init');		
+		this.githubInfo = {};			
+		this.githubInfo = new Github(options.user);
+	};
+	
+	this.execute = function(options){
+		console.log('GithubPlugin::e');
+		githubInfo.getRepoTags(options.user, options.repo, $.proxy(this._getRepoTags_successHandler, this));							
+	};
+	
+	this._getRepoTags_successHandler = function(meta){
+		console.log('GithubPlugin::getRepoTags->', result);		
+	};
+	
+	this._getCallback = function (data) {
+		if (this.callback != null){
+			this.callback(data);
+			this.callback = null;
+		}
+	};	
+}
