e482f128b06b5d241a1678deae037a4b02015202
Ozipi <ozipi.nomad@gmail.com>
2012-01-28 15:05:07 -0600

    Updated plugin options

--- examples/index.js
+++ examples/index.js
@@ -2,7 +2,7 @@ $(function(){
 	//execute the gertrudis widget
 	var options = {};
 	options.skin = 'BasicSkin';
-	options.pluginOptions = {repo: 'brp', user: 'ozipi'};
+	options.pluginOptions = {repo: 'gertrudis', user: 'ozipi', baseToCompare: 150, versionToTrack: 1};
 	$('#testDiv').gertrudis(options);
 });
 
--- plugins/github.plugin.js
+++ plugins/github.plugin.js
@@ -6,6 +6,7 @@ function GithubPlugin (options){
 		this.githubInfo = {};			
 		this.githubInfo = new Github(this.options.user);
 		this.callback = null;
+		(this.options.baseToCompare == undefined)? this.options.baseToCompare = 100 : null;	
 	};
 	
 	this.execute = function(options, handler){
@@ -24,28 +25,51 @@ function GithubPlugin (options){
 		console.log('GithubPlugin::getProjectPercentages', pData);
 		var data = {};
 		var topVersion = 0;
+		var results = {};
 		for (var i=0; i < pData.length; i++) {
-			var perct = this.getCompletitionData(pData[i].name);
-			(topVersion < perct)? topVersion = perct : null;			
-			console.log('pData[i]:', pData[i].name, perct, topVersion);
+			var itemData = this.getCompletitionData(pData[i].name);
+			console.log('pData[i]:', itemData, results[itemData.major]);			
+			if (results[itemData.major] != undefined){
+				console.log('defined', results[itemData.major].min, itemData.min);
+				if (results[itemData.major].min < itemData.min){
+					console.log('> !');
+					results[itemData.major] = itemData;
+				}
+			}
+			else{
+				console.log('undefined');
+				results[itemData.major] = itemData;
+			}
+			
 		};
 		
-		data.completition = topVersion;
-		return data;
+		console.log('result:', results);	
+		
+		return results;
 	};
 	
 	this.getCompletitionData = function(data){
 		//Static vX.xx
 		var dotPos = data.indexOf('.');
-		var version = data.substr(dotPos, data.length);
-		version = version * 100;
+		var vPos = data.indexOf('v');		
+		var majorVersion = data.substr((vPos + 1), (dotPos -1));		
+		var minVersion = data.substr(dotPos, data.length);
+		var completition = minVersion * 100;
+		var pending = this.options.baseToCompare - completition;
+		
 		//console.log('version->', version);
+		var version = {major: majorVersion, min: minVersion, completition: completition, pending: pending};
 		return version;
 	};
 	
 	this._getCallback = function (data) {
 		if (this.callback != null){
-			this.callback(data);
+			if (this.options.versionToTrack != undefined){
+				this.callback(data[this.options.versionToTrack]);				
+			}
+			else{
+				this.callback(data);				
+			}
 			this.callback = null;
 		}
 	};	
